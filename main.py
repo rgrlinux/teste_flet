5e03557f603a740936e897936c96d53520378ba6
rgrlinux <rgrlinux@gmail.com>
2024-04-23 11:14:52 -0300

    add new field

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def main(page: ft.page):
     product_images = ft.Container(
         bgcolor=ft.colors.WHITE,
         padding=ft.padding.all(30),
-        aspect_ratio=9/16,
+        aspect_ratio=9 / 16,
         content=ft.Column(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                main_image:=ft.Image(
+                main_image := ft.Image(
                     src='assets/img/couch_1.png',
                 ),
-                options:=ft.Row(
+                options := ft.Row(
                     alignment=ft.MainAxisAlignment.CENTER,
                     controls=[
                         ft.Container(
@@ -57,7 +57,7 @@ def main(page: ft.page):
     product_details = ft.Container(
         padding=ft.padding.all(30),
         bgcolor=ft.colors.BLACK87,
-        aspect_ratio=9/16,
+        aspect_ratio=9 / 16,
         content=ft.Column(
             controls=[
                 ft.Text(
@@ -74,13 +74,14 @@ def main(page: ft.page):
                 ft.Text(
                     value='Living Room',
                     color=ft.colors.GREY,
-                    italic=True
+                    italic=True,
+
                 ),
                 ft.ResponsiveRow(
                     columns=12,
                     controls=[
                         ft.Text(
-                            col={'xs':12, 'sm': 6},
+                            col={'xs': 12, 'sm': 6},
                             value='R$ 339',
                             color=ft.colors.WHITE,
                             size=28),
@@ -90,8 +91,60 @@ def main(page: ft.page):
                             color=ft.colors.AMBER if _ < 4 else ft.colors.WHITE,
                         ) for _ in range(5)]),
                     ]
-                )
-
+                ),
+                ft.Tabs(
+                    selected_index=0,
+                    height=150,
+                    indicator_color=ft.colors.AMBER,
+                    label_color=ft.colors.AMBER,
+                    unselected_label_color=ft.colors.GREY,
+                    tabs=[
+                        ft.Tab(
+                            text='Description',
+                            content=ft.Container(
+                                padding=ft.padding.all(10),
+                                content=ft.Text(
+                                    value= 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.' +
+                                    'De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo ' +
+                                    'vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. ' +
+                                    'Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi ' +
+                                    'dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat ' +
+                                    'max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat ' +
+                                    'mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go ' +
+                                    'lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. ' +
+                                    'Nigh tofth eliv ingdead.',
+                                    color=ft.colors.GREY,
+                                )
+                            )
+                        ),
+                        ft.Tab(
+                            text='Details',
+                            content=ft.Container(
+                                padding=ft.padding.all(10),
+                                content=ft.Text(
+                                    value='Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.' +
+                                          'De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo ' +
+                                          'vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. ' +
+                                          'Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi ' +
+                                          'mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go ' +
+                                          'lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. ' +
+                                          'Nigh tofth eliv ingdead.',
+                                    color=ft.colors.GREY,
+                                )
+                            )
+                        ),
+                        ft.ResponsiveRow(
+                            columns=12,
+                            controls=[
+                                ft.Dropdown(
+                                    col=6,
+                                    label='Color',
+                                    label_style=ft.TextStyle(),
+                                )
+                            ]
+                        ),
+                    ]
+                ),
             ]
         )
     )
@@ -104,10 +157,10 @@ def main(page: ft.page):
             spacing=0,
             run_spacing=0,
             controls=[
-                ft.Column(col=6, controls=[product_images,]),
-                ft.Column(col=6, controls=[product_details,]),
+                ft.Column(col=6, controls=[product_images, ]),
+                ft.Column(col=6, controls=[product_details, ]),
             ]
-          )
+        )
 
     )
     page.add(layout)
